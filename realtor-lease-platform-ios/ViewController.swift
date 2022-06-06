dce94e825ee0d0b2c9801708d36070c6850bb9b2
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-06-06 22:16:07 +0800

    webview init

--- realtor-lease-platform-ios/ViewController.swift
+++ realtor-lease-platform-ios/ViewController.swift
@@ -6,12 +6,29 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , WKScriptMessageHandler  {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        
+    }
+    
+    var webView: WKWebView? = nil
+    var url = "http://34.81.209.11:8080"
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("hello")
+        let contentController = WKUserContentController();
+        let config = WKWebViewConfiguration()
+        config.userContentController = contentController
+        config.websiteDataStore = WKWebsiteDataStore.default()
+        self.webView = WKWebView(
+                    frame: CGRect(x:0, y:20, width: self.view.frame.width, height:self.view.frame.height-20),
+                    configuration: config
+                )
+        let request = URLRequest(url: URL(string: url)!)
+        //http://34.81.209.11:8080/
+        self.webView!.load(request)
+        self.view.addSubview(self.webView!)
         // Do any additional setup after loading the view.
     }
 
