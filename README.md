93ac78ca5875dedd26690c2f34b17054755fb0de
chengyen.chiang <chengyen.chiang@leediancn.com>
2022-06-06 21:34:43 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# realtor-lease-platform-ios
---
+++ realtor-lease-platform-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realtor-lease-platform-ios
+//
+//  Created by 蔣政言 on 2022/6/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
